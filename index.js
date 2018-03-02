5ca6c31a225894fabe092c570cd867d8640454c0
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-02 18:41:39 +0700

    connect

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const reload = require('reload');
 
 const app = express();
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/a.html'));
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('a');
+});
+
+const arrWords = [
+    { en: 'one', vn: 'mot' },
+    { en: 'two', vn: 'hai' },
+    { en: 'three', vn: 'ba' },
+];
+
+app.get('/words', (req, res) => res.render('words', { words: arrWords }));
+//npm install nodemon@1.16.1 -g
 app.listen(3000, () => console.log('Server started!'));
+reload(app);
